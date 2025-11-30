5e807816b49b4bf2b48bc9eacd309d8f77520f31
Suleymanov <sierra19qwe@gmail.com>
2025-11-30 14:09:28 +0300

    feat: kuyruk.txt dosyasi eklendi

--- _22670310221_SuleymanSuleymanov.java
+++ _22670310221_SuleymanSuleymanov.java
@@ -2,4 +2,16 @@ public class _22670310221_SuleymanSuleymanov {
     public static void main(String[] args) {
 
     }
+
+    public class kuyruk {
+        int capacity;
+        Object[] array;
+        int size;
+        int front;
+        int rear;
+
+        public kuyruk() {
+
+        }
+    }
 }
