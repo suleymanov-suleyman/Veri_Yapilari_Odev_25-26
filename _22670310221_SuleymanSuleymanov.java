1f44d73ac6a20f1062454a553cbd141ee40a5577
Suleymanov <sierra19qwe@gmail.com>
2025-11-30 15:12:29 +0300

    feat: Kuyruk classi constructor ile olusturuldu ve main metodunda dosya okuma kisminda artik ilk 2 satir okunuyor

--- _22670310221_SuleymanSuleymanov.java
+++ _22670310221_SuleymanSuleymanov.java
@@ -1,17 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class _22670310221_SuleymanSuleymanov {
     public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("kuyruk.txt"))) {
+            int capacity = Integer.parseInt(reader.readLine().trim());
+            Kuyruk kuyruk = new Kuyruk(capacity);
+
+            List<String> tipler = Arrays.asList(reader.readLine().split(" "));
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Dosya Bulunamadi!");
+            e.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
 
     }
 
-    public class kuyruk {
+    public static class Kuyruk {
         int capacity;
         Object[] array;
         int size;
         int front;
         int rear;
 
-        public kuyruk() {
-
+        public Kuyruk(int capacity) {
+            this.capacity = capacity;
+            this.array = new Object[capacity];
+            this.front = 0;
+            this.rear = -1;
+            this.size = 0;
         }
     }
 }
